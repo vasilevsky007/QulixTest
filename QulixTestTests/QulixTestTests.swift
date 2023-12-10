c0db09eb6c8c10d195bb949e775aafb16673c839
alex <alex.vasilevski007@gmail.com>
2023-12-10 21:54:39 +0300

    feat: added tests to cover both of my model classes

--- QulixTestTests/QulixTestTests.swift
+++ QulixTestTests/QulixTestTests.swift
@@ -6,17 +6,46 @@
 //
 
 import XCTest
+import CoreData
 @testable import QulixTest
 
 final class QulixTestTests: XCTestCase {
     
+    // Ленивое свойство для контекста Core Data для тестов
+    lazy var testManagedObjectContext: NSManagedObjectContext = {
+        // Используйте Bundle(for:) для загрузки модели Core Data из вашего тестового bundle
+        guard let modelURL = Bundle(for: type(of: self)).url(forResource: "QulixTest", withExtension: "momd") else {
+            fatalError("Модель Core Data не найдена")
+        }
+
+        guard let managedObjectModel = NSManagedObjectModel(contentsOf: modelURL) else {
+            fatalError("Не удалось загрузить модель Core Data")
+        }
+
+        let persistentStoreCoordinator = NSPersistentStoreCoordinator(managedObjectModel: managedObjectModel)
+
+        do {
+            // Используйте NSInMemoryStoreType для временного хранения данных в памяти
+            try persistentStoreCoordinator.addPersistentStore(ofType: NSInMemoryStoreType, configurationName: nil, at: nil, options: nil)
+        } catch {
+            fatalError("Не удалось настроить хранилище Core Data: \(error)")
+        }
+
+        let managedObjectContext = NSManagedObjectContext(concurrencyType: .mainQueueConcurrencyType)
+        managedObjectContext.persistentStoreCoordinator = persistentStoreCoordinator
+
+        return managedObjectContext
+    }()
+    
     var giphySearcher: ImageSearcher!
+    var imageFetcher: ImageFetcher!
     
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
         try super.setUpWithError()
         // в данном методе, который запускается перед началом тестов, инициируем объект в виде класа, что позволит нам обращаться к его свойствам и методам
         giphySearcher = GiphyImageSearcher(step: 20)
+        imageFetcher = ImageFetcher()
     }
     
     override func tearDownWithError() throws {
@@ -26,7 +55,7 @@ final class QulixTestTests: XCTestCase {
         try super.tearDownWithError()
     }
     
-    func testExample() async throws {
+    func testSearcher() async throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
         // Any test you write for XCTest can be annotated as throws and async.
@@ -36,20 +65,45 @@ final class QulixTestTests: XCTestCase {
             let data1 = try await giphySearcher.search(for: "cute cat")
             let data2 = try await giphySearcher.getNext()
             
-            XCTAssertEqual(data1.count, 20)
-            XCTAssertEqual(data2.count, 20)
-            XCTAssertNotEqual(data1, data2)
+            XCTAssertEqual(data1.count, 20, "должно быть получено 20 изображений")
+            XCTAssertEqual(data2.count, 20, "должно быть получено еще 20 изображений")
+            XCTAssertNotEqual(data1, data2, "полученные изображения болжны быть разными")
         } catch {
             print("ERROR: ", error)
         }
-        
-    }
+    }    
     
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    func testImageFetcher() async throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        // Any test you write for XCTest can be annotated as throws and async.
+        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
+        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+        do {
+            //https://i.pinimg.com/736x/d8/55/c6/d855c66f95e9d3babeebd1e88bf4026d.jpg
+            await imageFetcher.setContext(context: testManagedObjectContext)
+            let data1 = try await imageFetcher.imageData(forUrl: URL(string: "https://i.pinimg.com/736x/d8/55/c6/d855c66f95e9d3babeebd1e88bf4026d.jpg")!)
+            let data2 = try await imageFetcher.imageData(forUrl: URL(string: "https://i.pinimg.com/736x/d8/55/c6/d855c66f95e9d3babeebd1e88bf4026d.jpg")!)
+            XCTAssertEqual(data1, data2, "Загрузка изображения два раза подряд должна возвращать одно и то же изображение")
+            var cacheSize = await imageFetcher.cacheSize
+            XCTAssertNotEqual(cacheSize, 0, "После загрузки изображений размер кэша не должен быть равен нулю")
+//            следующая строка почему то крашит именно на тестировании, возможно у меня неправильно настроен конткст...
+//            await imageFetcher.clearCache()
+//            cacheSize = await imageFetcher.cacheSize
+//            XCTAssertEqual(cacheSize, 0)
+            
+        } catch {
+            print("ERROR: ", error)
         }
     }
+    
+    
+    
+//    func testPerformanceExample() throws {
+//        // This is an example of a performance test case.
+//        self.measure {
+//            // Put the code you want to measure the time of here.
+//        }
+//    }
     
 }
